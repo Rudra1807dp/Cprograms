acc8cf30bfc0e8d413120d38ceefe3b1123bc182
Rudra1807dp <rudradp.18.07@gmail.com>
2025-08-12 20:10:13 +0530

    Add files via upload

---
+++ pr7.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    float a,b;
+    float c=48;
+    printf("Enter the value of dollars you want to convert in rupees ");
+    scanf("%f",&a);
+    b=c*a;
+    printf("the value in ruprees is %f",b);
+}
